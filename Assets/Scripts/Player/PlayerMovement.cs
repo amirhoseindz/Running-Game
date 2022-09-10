9bfa54c6a179835793fb60ca401a9709ae064c6f
unknown <amir.mh17@yahoo.com>
2022-09-10 21:29:31 +0430

    add a feature to the game to make slide moves for with mouse pointer

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,23 +7,48 @@ public class PlayerMovement : MonoBehaviour
 {
     private float _moveSpeed = 1.5f;
     private float _movingAsideSpeed = 2f;
-    
+    private Vector3 _tempPos;
+    private Camera _cam;
+    private Collider _roadCollider;
+    private RaycastHit _hit;
+    private Ray _ray;
+
+    private void Start()
+    {
+        _cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        _roadCollider = GameObject.Find("Road").GetComponent<Collider>();
+    }
+
     void Update()
     {
-        transform.Translate(  Time.deltaTime * _moveSpeed * Vector3.forward, Space.World);
+        transform.Translate (Time.deltaTime * _moveSpeed * Vector3.forward, Space.World);
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             if (gameObject.transform.position.x > RoadBoundaries.leftSide)
             {
-                transform.Translate(  Time.deltaTime * _movingAsideSpeed * Vector3.left);
+                transform.Translate (Time.deltaTime * _movingAsideSpeed * Vector3.left);
             }
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             if (gameObject.transform.position.x < RoadBoundaries.rightSide)
             {
-                transform.Translate(  Time.deltaTime * _movingAsideSpeed * -Vector3.left);
+                transform.Translate (Time.deltaTime * _movingAsideSpeed * -Vector3.left);
+            }
+        }
+        if (gameObject.transform.position.x < RoadBoundaries.rightSide
+            && gameObject.transform.position.x > RoadBoundaries.leftSide)
+        {
+            _ray = _cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(_ray, out _hit))
+            {
+                if (_hit.collider == _roadCollider)
+                {
+                    _tempPos = _hit.point;
+                    _tempPos.y = 0.3f;
+                    transform.position = Vector3.MoveTowards(transform.position, _tempPos, Time.deltaTime);
+                }
             }
         }
     }
