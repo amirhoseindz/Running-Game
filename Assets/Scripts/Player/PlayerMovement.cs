dc71399bc5092cc44f8865b62961fd0617afdd06
unknown <amir.mh17@yahoo.com>
2022-09-13 21:23:35 +0430

    solve the problem that player wouldnt follow the mouse after section one

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public  Collider _roadCollider;
+    
     private float _moveSpeed = 1.5f;
     private float _movingAsideSpeed = 2f;
     private Vector3 _tempPos;
     private Camera _cam;
-    public Collider _roadCollider;
     private RaycastHit _hit;
     private Ray _ray;
 
     private void Start()
     {
         _cam = GameObject.Find("Main Camera").GetComponent<Camera>();
-        _roadCollider = GameObject.Find("Road").GetComponent<Collider>();
     }
 
     void Update()
@@ -43,12 +43,9 @@ public class PlayerMovement : MonoBehaviour
             _ray = _cam.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(_ray, out _hit))
             {
-                if (_hit.collider == _roadCollider)
-                {
-                    _tempPos = _hit.point;
-                    _tempPos.y = 0.3f;
-                    transform.position = Vector3.MoveTowards(transform.position, _tempPos, Time.deltaTime);
-                }
+                _tempPos = _hit.point;
+                _tempPos.y = 0.3f;
+                transform.position = Vector3.MoveTowards(transform.position, _tempPos, Time.deltaTime);
             }
         }
     }
